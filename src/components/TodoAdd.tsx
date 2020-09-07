36699d3b6446730a6419e27c25b90f72ccbec060
PD Jell <mystral7@naver.com>
2020-09-07 16:33:50 +0900

    Complete TodoAdd.

--- src/components/TodoAdd.tsx
+++ src/components/TodoAdd.tsx
@@ -1,3 +1,33 @@
 import React, { useState } from 'react'
 import { kButton, kTodoInput, kInputFormWrap } from '../contstants'
 
+interface TodoAddProps {
+  addTodo: (content: string) => void
+}
+
+const TodoAdd: React.FC<TodoAddProps> = ({ addTodo }) => {
+  const [content, setContent] = useState('')
+  const changeContent = (event: any) => setContent(event.target.value)
+  const submit = (event: any) => {
+    event.preventDefault()
+    addTodo(content)
+    setContent('')
+  }
+
+  return (
+    <>
+      <form onSubmit={submit} style={kInputFormWrap}>
+        <input
+          type="text"
+          style={kTodoInput}
+          value={content}
+          onChange={changeContent}
+        />
+        <button type="submit" style={kButton}>추가</button>
+      </form>
+      <hr style={{ width: '100%' }} />
+    </>
+  )
+}
+
+export default React.memo(TodoAdd)
