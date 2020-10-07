75520503ac1561eebb9ef099af6bae3da6826b84
PD Jell <mystral7@naver.com>
2020-10-07 16:56:53 +0900

    resolve optional.

--- src/pages/TodoPage.tsx
+++ src/pages/TodoPage.tsx
@@ -5,7 +5,7 @@ import TodoList from '../containers/TodoList'
 import { Subscription } from 'rxjs'
 
 const TodoPage: React.FC = () => {
-  const [todos, setTodos] = useState()
+  const [todos, setTodos] = useState(Array<TodoItem>())
 
   useEffect(() => { // useEffect는 didmount, didupdate, willunmount가 합쳐진 것으로 생각해도 됨. eg:https://ko.reactjs.org/docs/hooks-effect.html
     const todoData$: Subscription = TodoService.todoData$
